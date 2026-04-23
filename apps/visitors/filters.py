23fcec9792298521528bc58359f48cfe12df1edf
Ridwan241 <mohammadridwan241@gmail.com>
2026-04-24 00:50:57 +0600

    first commit

--- apps/visitors/filters.py
+++ apps/visitors/filters.py
@@ -4,11 +4,11 @@ from .models import Visit
 
 
 class VisitFilter(django_filters.FilterSet):
-    date_from = django_filters.DateFilter(field_name='visit_date', lookup_expr='gte')
-    date_to = django_filters.DateFilter(field_name='visit_date', lookup_expr='lte')
-    visitor_name = django_filters.CharFilter(field_name='visitor_name', lookup_expr='icontains')
-    host_name = django_filters.CharFilter(field_name='host_name', lookup_expr='icontains')
-    phone = django_filters.CharFilter(field_name='visitor_phone', lookup_expr='icontains')
+    date_from = django_filters.DateFilter(field_name='visit_date', lookup_expr='gte', label='From date')
+    date_to = django_filters.DateFilter(field_name='visit_date', lookup_expr='lte', label='To date')
+    visitor_name = django_filters.CharFilter(field_name='visitor_name', lookup_expr='icontains', label='Visitor')
+    host_name = django_filters.CharFilter(field_name='host_name', lookup_expr='icontains', label='Host')
+    phone = django_filters.CharFilter(field_name='visitor_phone', lookup_expr='icontains', label='Phone')
 
     class Meta:
         model = Visit
